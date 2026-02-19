63fc248c47eb94bf8301822544aa30d2ec5047fd
Phinehas Adams <phinehasmadams@icloud.com>
2026-02-19 10:52:24 -0600

    Further reduce header logo size

--- src/components/site-header.tsx
+++ src/components/site-header.tsx
@@ -25,7 +25,7 @@ export function SiteHeader() {
               alt="Havi's Candy Co. logo"
               width={628}
               height={412}
-              sizes="(max-width: 640px) 70px, (max-width: 1024px) 84px, 100px"
+              sizes="(max-width: 640px) 68px, (max-width: 1024px) 82px, 95px"
               priority
               className="logo-image site-header-logo-image"
             />
